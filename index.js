86a86692071d0355c6b9aaeb1138156a452960dc
Alexandria <alex.pugia816@hotmail.com>
2019-10-07 14:35:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+const messages = []
+
+function writeCards(name, event) {
+    for (let i = 0; i < name.length; i++) {
+        messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages
+}
+
+function countdown(number){
+    while (number > -1) {
+        console.log(number)
+        number -= 1
+    }
+}
